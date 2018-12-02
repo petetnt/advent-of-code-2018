1204de1a4bf0ed4c9c9e4479bdd31234822669c0
Pete Nykänen <pete.a.nykanen@gmail.com>
2018-12-02 19:39:28 +0200

    Solve 2.2

--- src/2/index.js
+++ src/2/index.js
@@ -1,4 +1,5 @@
 import input from "./input";
+import fl from "fast-levenshtein";
 
 const run = () => {
   // part 1
@@ -34,4 +35,31 @@ const run = () => {
   console.log("Part 2.1", a * b);
 };
 
+const run2 = () => {
+  const pairwise = list => {
+    if (list.length < 2) {
+      return [];
+    }
+    const first = list[0];
+    const rest = list.slice(1);
+    const pairs = rest.map(x => [first, x]);
+    return pairs.concat(pairwise(rest));
+  };
+
+  const pairs = pairwise(input);
+  const match = pairs.find(([a, b]) => fl.get(a, b) === 1);
+
+  const commonLetters = [];
+
+  for (let i = 0; i < match[0].length; i++) {
+    if (match[0][i] === match[1][i]) {
+      commonLetters.push(match[0][i]);
+    }
+  }
+
+  console.log(match);
+  console.log("Part 2.2", commonLetters.join(""));
+};
+
 run();
+run2();
