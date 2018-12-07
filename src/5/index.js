024691a3cc583560e62aff06447c26a08bec4fc0
Pete Nykänen <pete.a.nykanen@gmail.com>
2018-12-07 23:02:48 +0200

    Part 5.2.

--- src/5/index.js
+++ src/5/index.js
@@ -4,7 +4,7 @@ const arePolar = (a, b) => {
   return a.toLowerCase() === b.toLowerCase() && a.charAt(0) !== b.charAt(0);
 };
 
-const run = () => {
+const run = input => {
   let completed = false;
   let text = input;
   let loops = 0;
@@ -29,7 +29,29 @@ const run = () => {
     }
   }
 
-  console.log("Part 5.1", text.length);
+  return text.length;
 };
 
-run();
+const run2 = () => {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const re = letter => new RegExp(letter, "gi");
+
+  let lowest = Number.MAX_SAFE_INTEGER;
+
+  for (const i of letters) {
+    let test = input.replace(re(i), "");
+    const num = run(test);
+
+    if (num < lowest) {
+      lowest = num;
+    }
+  }
+
+  return lowest;
+};
+
+const one = run(input);
+console.log("Part 5.1", one);
+
+const two = run2();
+console.log("Part 5.2", two);
