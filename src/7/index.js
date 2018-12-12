9ac70714a5291f286efb5d062b42bf2ff5c5f977
Pete Nykänen <pete.a.nykanen@gmail.com>
2018-12-13 00:48:46 +0200

    Solve 7.2.

--- src/7/index.js
+++ src/7/index.js
@@ -11,33 +11,80 @@ const format = f => {
   }, {});
 };
 
-let i = format(input);
-
 const findKeys = i =>
   Object.entries(i)
     .filter(([key, val]) => val.length === 0)
     .sort(([a], [b]) => a.localeCompare(b));
 
-const final = [];
-
-while (Object.keys(i).length) {
-  const [[firstKey]] = findKeys(i);
-
-  const removeKey = (list, key) => {
-    const newList = Object.entries(list).reduce((x, [k, deps]) => {
-      if (k === key) {
-        final.push(k);
-        return x;
-      }
-
-      x[k] = deps.filter(i => i !== key);
+const removeKey = (list, key) => {
+  const newList = Object.entries(list).reduce((x, [k, deps]) => {
+    if (k === key) {
       return x;
-    }, {});
+    }
 
-    return newList;
-  };
+    x[k] = deps.filter(i => i !== key);
+    return x;
+  }, {});
 
-  i = removeKey(i, firstKey);
-}
+  return newList;
+};
 
-console.log("Part 7.1", final.join(""));
+const run = () => {
+  let i = format(input);
+
+  let final = [];
+
+  while (Object.keys(i).length) {
+    const [[firstKey]] = findKeys(i);
+    final.push(firstKey);
+    i = removeKey(i, firstKey);
+  }
+  console.log("Part 7.1", final.join(""));
+};
+
+const run2 = () => {
+  let i = format(input);
+
+  let timer = 0;
+  const KEYS_UNDER_WORK = new Set();
+  let WORKERS = [];
+  const MAX_WORKERS = 5;
+  const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // main loop
+  while (Object.keys(i).length) {
+    let availableKeys = findKeys(i).filter(
+      ([key]) => !KEYS_UNDER_WORK.has(key)
+    );
+
+    // assign keys to available workers
+    if (WORKERS.length < MAX_WORKERS) {
+      for (
+        let j = 0;
+        WORKERS.length < MAX_WORKERS && j < availableKeys.length;
+        j++
+      ) {
+        const [key] = availableKeys[j];
+        KEYS_UNDER_WORK.add(key);
+        WORKERS.push({ key, need: 60 + ALPHABET.indexOf(key) + 1, current: 0 });
+      }
+    }
+
+    // increment workers and remove keys if ready
+    WORKERS = WORKERS.reduce((acc, worker) => {
+      worker.current = worker.current + 1;
+      if (worker.current < worker.need) {
+        acc.push(worker);
+      } else {
+        KEYS_UNDER_WORK.delete(worker.key);
+        i = removeKey(i, worker.key);
+      }
+      return acc;
+    }, []);
+
+    timer++;
+  }
+  console.log("Part 7.2", timer);
+};
+
+run();
+run2();
